099db67593b0a1668603a701b2ebd2a70f4ba301
indianic <indianic@indianics-iMac-2.local>
2022-11-17 15:58:22 +0530

    pages create

--- assets/js/firebase-ourwork.js
+++ assets/js/firebase-ourwork.js
@@ -4,21 +4,17 @@ import firebaseConfig from '../../config/firebase-config.json' assert { type: "j
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
-const querySnapshot = await getDocs(collection(db, "about"));
+console.log("db", db)
+const querySnapshot = await getDocs(collection(db, "ourwork"));
+console.log("querySnapshot", querySnapshot)
 
 var numbers = [];
 querySnapshot.forEach((doc) => {
     numbers.push(doc.data())
 })
-if (navigator.onLine) {
 
-    localStorage.setItem("aboutObj", JSON.stringify(numbers));
-}
-
-const aboutArr = [...JSON.parse(localStorage.getItem("aboutObj"))];
-
-var adsadshasdhask = document.querySelector('#app');
-adsadshasdhask.innerHTML = '<ul>' + aboutArr.map(function (object) {
+var adsadshasdhask = document.querySelector('#ourworkId');
+adsadshasdhask.innerHTML = '<ul>' + numbers.map(function (object) {
     return '<li>' + object?.backgroundImage + '</li>';
 }).join('') + '</ul>';
+
--- src/main.js
+++ src/main.js
@@ -1,24 +1,38 @@
 // src/main.js
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const path = require('path')
 const env = process.env.NODE_ENV || 'development';
 
 function createWindow() {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
         width: 1200,
-        height: 800,
+        height: 800
+
     });
 
     mainWindow.loadFile('../pages/homepage.html');
+    mainWindow.webContents.openDevTools()
+
 }
 if (env === 'development') {
-    try {
-        require('electron-reloader')(module, {
-            debug: true,
-            watchRenderer: true
-        });
-    } catch (_) { console.log('Error'); }
+    require('electron-reloader')(module, {
+        // electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+        // hardResetMethod: 'exit',
+        debug: true,
+        watchRenderer: true
+    });
 }
 // After initialization, you can create browser windows.
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
